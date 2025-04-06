983a9adce713fa9b9e6dc8ccb12f8f910532e686
Lakshay <79142303+lakshayknows@users.noreply.github.com>
2025-04-06 15:12:17 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -5,6 +5,7 @@ const cols = 53;
 const cellSize = 15;
 
 const grid = [];
+let snakeClickCount = 0;
 
 for (let y = 0; y < rows; y++) {
   for (let x = 0; x < cols; x++) {
@@ -27,10 +28,23 @@ const snakeTrail = [];
 function updateSnake() {
   const { x, y } = grid[snakeIndex];
   const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+  // Easter egg on click
+  circle.addEventListener('click', () => {
+    snakeClickCount++;
+    if (snakeClickCount === 3) {
+      alert("🥚 You found the Easter Egg!\n🐍 Snake: 'Commit to your dreams, Lakshay.'");
+      snakeClickCount = 0;
+    } else {
+      alert(`🐍 Snake says hi, Lakshay! (${snakeClickCount}/3)`);
+    }
+  });
+
   circle.setAttribute("cx", x * cellSize + cellSize / 2);
   circle.setAttribute("cy", y * cellSize + cellSize / 2);
   circle.setAttribute("r", cellSize / 3);
   circle.setAttribute("class", "snake");
+
   svg.appendChild(circle);
   snakeTrail.push(circle);
 
