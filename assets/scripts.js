0b159e40b98f57c6b8f3310608d19db1bac27211
Lakshay <79142303+lakshayknows@users.noreply.github.com>
2025-07-03 16:25:48 +0530

    Delete assets directory

--- assets/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("assets/quote.json")
-    .then((res) => res.json())
-    .then((quotes) => {
-      const random = Math.floor(Math.random() * quotes.length);
-      document.getElementById("daily-quote").textContent = quotes[random];
-    });
-});
